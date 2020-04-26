52b12001d65cffe29949092edf94893ae9f039d9
alexandr <pakim.wesi@gmail.com>
2020-04-26 12:03:02 +0900

    Создание маски ввода

---
+++ src/components/input/input.js
@@ -0,0 +1,11 @@
+import * as $ from 'jquery';
+import Inputmask from 'inputmask';
+
+$(document).ready(function(){
+	Inputmask('datetime', {
+			inputFormat: 'dd.mm.yyyy', 
+			placeholder: 'ДД.ММ.ГГГГ'
+		}).mask('.input.input_mask-date .input__field');
+
+	Inputmask('email').mask('.input.input_mask-email .input__field');
+});
