bc6bb996f1a471a5e1baff565a5ff20a00ffab0f
alexandr <pakim.wesi@gmail.com>
2020-05-03 21:08:53 +0900

    Добавление раскрывающегося списка

---
+++ src/components/collapse/collapse.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+	$('body').on('click', '.collapse .collapse__btn', function(e){
+		e.preventDefault();
+		var $parent = $(this).closest('.collapse');
+		var collapse_group = $parent.attr('data-group');
+		
+		if( collapse_group != '' && collapse_group !== 'undefined' ){			
+			var selector = '.collapse';
+			
+			$('body').find(selector + '[data-group="' + collapse_group + '"]').each(function(){
+				if( $(selector).index( $(this) ) == $(selector).index($parent) ){
+					$parent.toggleClass('collapse_active');
+				} else{
+					$(this).removeClass('collapse_active');
+				}
+			});
+		} else{
+			$parent.toggleClass('collapse_active');
+		}
+	});
+});
