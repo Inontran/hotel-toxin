a1bed988890e306f14c76c0d4a822fe9019226c3
alexandr <pakim.wesi@gmail.com>
2020-05-20 23:04:53 +0900

    Разработка связки datepicker и dropdown

--- src/components/air-datepicker/air-datepicker.js
+++ src/components/air-datepicker/air-datepicker.js
@@ -5,7 +5,7 @@ moment.lang('ru');
 
 
 $(document).ready(function(){
-	$('body').find('.air-datepicker .air-datepicker__wrapper').each(function(){
+	$('body').find('.air-datepicker.air-datepicker_inline .air-datepicker__wrapper').each(function(){
 		$(this).datepicker({
 			range: true,
 			inline: true,
@@ -17,16 +17,19 @@ $(document).ready(function(){
 		});
 	});
 
+
 	$('body').on('click', '.air-datepicker .air-datepicker__btn-wrapper_clear', function(){
 		$(this).closest('.air-datepicker').find('.air-datepicker__wrapper').data('datepicker').clear();
 	});
+
 
 	$('body').on('click', '.air-datepicker .air-datepicker__btn-wrapper_submit', function(){
 		var dates = $(this).closest('.air-datepicker')
 											.find('.air-datepicker__wrapper')
 											.data('datepicker').selectedDates;
 
-		$('.input_result-dates .input__field').val(moment(dates[0]).format('D MMM') + ' - ' + moment(dates[1]).format('D MMM'));
+		$('.input_result-dates .input__field')
+			.val(moment(dates[0]).format('D MMM') + ' - ' + moment(dates[1]).format('D MMM'));
 		$('.input_result-date-start .input__field').val(moment(dates[0]).format('DD.MM.YYYY'));
 		$('.input_result-date-end .input__field').val(moment(dates[1]).format('DD.MM.YYYY'));
 
