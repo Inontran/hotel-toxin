30baa918c2176965b23e97586696bdb8592cfd58
alexandr <pakim.wesi@gmail.com>
2020-05-21 22:27:25 +0900

    Доделан dropdown с datepicker

--- src/components/air-datepicker/air-datepicker.js
+++ src/components/air-datepicker/air-datepicker.js
@@ -18,21 +18,22 @@ $(document).ready(function(){
 	});
 
 
-	$('body').on('click', '.air-datepicker .air-datepicker__btn-wrapper_clear', function(){
+	$('body').on('click', '.dropdown .air-datepicker .air-datepicker__btn-wrapper_clear', function(){
 		$(this).closest('.air-datepicker').find('.air-datepicker__wrapper').data('datepicker').clear();
 	});
 
 
 	$('body').on('click', '.air-datepicker .air-datepicker__btn-wrapper_submit', function(){
+		var $parent_dropdown = $(this).closest('.dropdown');
 		var dates = $(this).closest('.air-datepicker')
 											.find('.air-datepicker__wrapper')
 											.data('datepicker').selectedDates;
 
-		$('.input_result-dates .input__field')
+		$('.input_result-dates .input__field', $parent_dropdown)
 			.val(moment(dates[0]).format('D MMM') + ' - ' + moment(dates[1]).format('D MMM'));
-		$('.input_result-date-start .input__field').val(moment(dates[0]).format('DD.MM.YYYY'));
-		$('.input_result-date-end .input__field').val(moment(dates[1]).format('DD.MM.YYYY'));
+		$('.input_result-date-start .input__field', $parent_dropdown).val(moment(dates[0]).format('DD.MM.YYYY'));
+		$('.input_result-date-end .input__field', $parent_dropdown).val(moment(dates[1]).format('DD.MM.YYYY'));
 
-		console.log( $(this).closest('.air-datepicker').find('.air-datepicker__wrapper').data('datepicker').inline );
+		$parent_dropdown.removeClass('dropdown_active');
 	});
 });
