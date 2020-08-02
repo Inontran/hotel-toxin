e463ac42d7ab84828f0fcea4281c07caa0b53d0f
alexandr <pakim.wesi@gmail.com>
2020-08-02 22:52:05 +0900

    Адаптация страницы product

---
+++ src/pages/product/product.js
@@ -0,0 +1,12 @@
+import 'owl.carousel';
+
+$(document).ready(function(){
+	$('.product__carousel', 'body').each(function(){
+		$(this).owlCarousel({
+			nav: true,
+			dots: true,
+			items: 1,
+			loop: false,
+		});
+	});
+});
