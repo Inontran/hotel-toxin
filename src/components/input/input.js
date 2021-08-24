6abb68ecb8ccb4a1490a950b0fb746bfc6defc6d
Inontran <pakim.wesi@gmail.com>
2021-08-24 12:54:50 +0900

    #17 Добавление placeholder для полей ввода даты

--- src/components/input/input.js
+++ src/components/input/input.js
@@ -2,10 +2,14 @@ import $ from 'jquery';
 import Inputmask from 'inputmask';
 
 $(() => {
+  const $inputsDate = $('.js-input.input_mask-date .js-input__field');
+
   Inputmask('datetime', {
     inputFormat: 'dd.mm.yyyy',
     placeholder: 'ДД.ММ.ГГГГ',
-  }).mask('.js-input.input_mask-date .js-input__field');
+  }).mask($inputsDate);
+
+  $inputsDate.attr('placeholder', 'ДД.ММ.ГГГГ');
 
   Inputmask('email').mask('.js-input.input_mask-email .js-input__field');
 });
