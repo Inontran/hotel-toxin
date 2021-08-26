b3a06493055285971dee2d7547c07d32ea358f1e
Inontran <pakim.wesi@gmail.com>
2021-08-26 17:44:41 +0900

    #17 Доработан блок отзыва и кнопки лайка

---
+++ src/components/review/review.js
@@ -0,0 +1,22 @@
+import $ from 'jquery';
+
+$(() => {
+  const $body = $('body');
+
+  $body.on('change', '.js-review .js-like-btn__check', (event) => {
+    const $likeBtnInput = $(event.currentTarget);
+    const $review = $likeBtnInput.closest('.js-review');
+    if (!$review.length) {
+      return;
+    }
+
+    $review.addClass('js-review_not-iterable');
+    const reviewId = $review.attr('data-review-id');
+    const $clonesCurrentReview = $(`.js-review:not(.js-review_not-iterable)[data-review-id="${reviewId}"]`);
+    if ($clonesCurrentReview.length) {
+      const isCheckLikeBtn = $likeBtnInput.prop('checked');
+      $('.js-like-btn__check', $clonesCurrentReview).prop('checked', isCheckLikeBtn).trigger('change');
+    }
+    $review.removeClass('js-review_not-iterable');
+  });
+});
