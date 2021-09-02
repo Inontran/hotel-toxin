fc45c489887adc02833df475a2138643df07a518
Inontran <pakim.wesi@gmail.com>
2021-09-02 12:20:07 +0900

    #19 Переделан обратно header и first-display.

---
+++ src/components/first-display/first-display.js
@@ -0,0 +1,32 @@
+import $ from 'jquery';
+
+$(() => {
+  const $headerSite = $('.js-header');
+  const $firstDisplay = $('.js-first-display');
+  if (!$firstDisplay.length) {
+    return;
+  }
+
+  const isExistHeader = $headerSite.length;
+  const heightFirstDisplayEqual100vh = $firstDisplay[0].offsetHeight === window.innerHeight;
+  const resultConditions = isExistHeader && heightFirstDisplayEqual100vh;
+  if (resultConditions) {
+    function calcHeightFirstDisplay(){
+      const headerHeight = $headerSite.outerHeight();
+      $firstDisplay.css('min-height', `calc(100vh - ${headerHeight}px)`);
+    }
+  
+    calcHeightFirstDisplay();
+  
+    let timerWindowResize = null;
+  
+    $(window).resize(() => {
+      if(timerWindowResize !== null) {
+        clearTimeout(timerWindowResize);        
+      }
+      timerWindowResize = setTimeout(function() {
+        calcHeightFirstDisplay();
+      }, 10);
+    });
+  }
+});
