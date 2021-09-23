ce06ed694d4213948daef98f43d360163611c17b
Inontran <pakim.wesi@gmail.com>
2021-09-23 21:48:52 +0900

    #17 Доработка сброса и инициализации календаря

--- src/components/air-datepicker/air-datepicker.js
+++ src/components/air-datepicker/air-datepicker.js
@@ -8,6 +8,20 @@ require('@/components/button/button');
 moment.lang('ru');
 
 $(() => {
+  function formatDate(strDate){
+    if (!strDate.length) {
+      return;
+    }
+
+    try {
+      const array = strDate.split('.', 3).reverse();
+      return array.join('-');
+    } catch (error) {
+      console.debug(error);
+    }
+    return;
+  }
+
   const $body = $('body');
 
   $body.find('.js-air-datepicker.air-datepicker_inline').each(function () {
@@ -20,11 +34,12 @@ $(() => {
       try {
         minDateOption = new Date(attrMinDate);
       } catch (error) {
-        console.log(error);
+        console.debug(error);
       }
     }
 
-    $('.js-air-datepicker__wrapper', $airDatepicker).datepicker({
+    const $datepicker = $('.js-air-datepicker__wrapper', $airDatepicker);
+    $datepicker.datepicker({
       range: true,
       inline: true,
       navTitles: {
@@ -32,6 +47,33 @@ $(() => {
       },
       minDate: minDateOption,
     });
+
+    let $parentDropdown = $airDatepicker.closest('.js-dropdown');
+    const targetDropdowns = $airDatepicker.attr('data-target-dropdown');
+    if (targetDropdowns) {
+      $parentDropdown = $(targetDropdowns);
+    }
+    if ($parentDropdown.length) {
+      const datesFromInputs = [];
+      if ($('.js-input_result-date-start .js-input__field', $parentDropdown).length) {
+        let valueInputDate = $('.js-input_result-date-start .js-input__field', $parentDropdown).val();
+        valueInputDate = formatDate(valueInputDate);
+        if (valueInputDate) datesFromInputs.push(new Date(valueInputDate)); 
+      }
+      if ($('.js-input_result-date-end .js-input__field', $parentDropdown).length) {
+        let valueInputDate = $('.js-input_result-date-end .js-input__field', $parentDropdown).val();
+        valueInputDate = formatDate(valueInputDate);
+        if (valueInputDate) datesFromInputs.push(new Date(valueInputDate)); 
+      }
+
+      if (datesFromInputs.length) {
+        $datepicker.data('datepicker').selectDate(datesFromInputs);
+      }
+    }
+    
+    if (!$datepicker.data('datepicker').selectedDates.length) {
+      $('.js-air-datepicker__btn-wrapper_reset', $airDatepicker).addClass('air-datepicker_hidden');
+    }
   });
 
   function handlerClickClearBtn(event) {
@@ -39,11 +81,23 @@ $(() => {
     const $btn = $(event.currentTarget);
     const $airDatepicker = $btn.closest('.js-air-datepicker');
     $airDatepicker.find('.js-air-datepicker__wrapper').data('datepicker').clear();
+
+    let $parentDropdown = $airDatepicker.closest('.js-dropdown');
+    const targetDropdowns = $airDatepicker.attr('data-target-dropdown');
+    if (targetDropdowns) {
+      $parentDropdown = $(targetDropdowns);
+    }
+    if ($parentDropdown.length) {
+      $('.js-input_result-date-start .js-input__field', $parentDropdown).val('');
+      $('.js-input_result-date-end .js-input__field', $parentDropdown).val('');
+    }
+
+    $btn.closest('.js-air-datepicker__btn-wrapper_reset').addClass('air-datepicker_hidden');
   }
 
   $body.on(
     'click',
-    '.js-dropdown .js-air-datepicker .js-air-datepicker__btn-wrapper_clear .js-button',
+    '.js-air-datepicker .js-air-datepicker__btn-wrapper_reset .js-button',
     handlerClickClearBtn,
   );
 
@@ -64,6 +118,12 @@ $(() => {
       $('.js-input_result-date-start .js-input__field', $parentDropdown).val(moment(dates[0]).format('DD.MM.YYYY'));
       $('.js-input_result-date-end .js-input__field', $parentDropdown).val(moment(dates[1]).format('DD.MM.YYYY'));
       $parentDropdown.removeClass('dropdown_active');
+    }
+
+    if (dates.length) {
+      $('.js-air-datepicker__btn-wrapper_reset', $airDatepicker).removeClass('air-datepicker_hidden');
+    } else {
+      $('.js-air-datepicker__btn-wrapper_reset', $airDatepicker).addClass('air-datepicker_hidden');
     }
   }
 
