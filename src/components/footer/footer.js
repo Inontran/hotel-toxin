ca5affd96edaa797b29f64c74ac5cec25f93014b
Inontran <pakim.wesi@gmail.com>
2021-10-07 17:07:08 +0900

    #19 Доработка верстки для проверки валидатором.

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -2,3 +2,4 @@ require('./footer.scss');
 require('@/components/logo/logo');
 require('@/components/social-nets/social-nets');
 require('@/components/input-text/input-text');
+require('@/components/heading/heading');
