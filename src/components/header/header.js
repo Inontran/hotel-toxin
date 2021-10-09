3a8316e25a8945ba79abf6daaa2625e1605a9052
Inontran <pakim.wesi@gmail.com>
2021-10-09 10:31:28 +0900

    #19 Переделано главное меню в шапке.

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,4 +1,25 @@
-require('./header.scss');
+import $ from 'jquery';
+
 require('@/components/logo/logo');
 require('@/components/button/button');
 require('@/components/dropdown/dropdown');
+
+require('./header.scss');
+
+$(() => {
+  const $body = $('body');
+
+  $body.on('click', '.js-header .js-header__btn_show-mainmenu', function(event){
+    const $btn = $(event.currentTarget);
+    const $header = $btn.closest('.js-header');
+    $header.removeClass('header_shown-auth-btns');
+    $header.toggleClass('header_shown-mainmenu');
+  });
+
+  $body.on('click', '.js-header .js-header__btn_show-auth-btns', function(event){
+    const $btn = $(event.currentTarget);
+    const $header = $btn.closest('.js-header');
+    $header.removeClass('header_shown-mainmenu');
+    $header.toggleClass('header_shown-auth-btns');
+  });
+});
