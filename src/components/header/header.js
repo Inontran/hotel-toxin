2d4ba52df16b2af62575ad2f0c75360188e4b8ba
Inontran <pakim.wesi@gmail.com>
2021-10-12 12:04:20 +0900

    #19 Переписаны некоторые классы элементов в 'header'.

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -13,17 +13,17 @@ $(() => {
     const $btn = $(event.currentTarget);
     const $header = $btn.closest('.js-header');
     $header.removeClass('header_shown-auth-btns');
-    $header.toggleClass('header_shown-mainmenu');
+    $header.toggleClass('header_shown-main-menu');
   }
 
-  $body.on('click', '.js-header .js-header__btn_show-mainmenu', handleBtnShowMainmenuClick);
+  $body.on('click', '.js-header .js-header__icon-btn_type_menu', handleBtnShowMainmenuClick);
 
   function handleBtnShowAuthBtnsClick(event) {
     const $btn = $(event.currentTarget);
     const $header = $btn.closest('.js-header');
-    $header.removeClass('header_shown-mainmenu');
+    $header.removeClass('header_shown-main-menu');
     $header.toggleClass('header_shown-auth-btns');
   }
 
-  $body.on('click', '.js-header .js-header__btn_show-auth-btns', handleBtnShowAuthBtnsClick);
+  $body.on('click', '.js-header .js-header__icon-btn_type_profile', handleBtnShowAuthBtnsClick);
 });
